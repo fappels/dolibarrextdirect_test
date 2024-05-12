87808c33001402b221ba0245194e2f71da63574a
fappels <francis.appels@yahoo.com>
2024-05-12 13:49:39 +0200

    work on extjs7

--- app/model/InterventionLine.js
+++ app/model/InterventionLine.js
@@ -10,40 +10,41 @@ Ext.define('ConnectorTest.model.InterventionLine', {
         'Ext.data.Field'
     ],
 
-    config: {
-        fields: [
-            {
-                name: 'id'
-            },
-            {
-                name: 'local_id'
-            },
-            {
-                name: 'line_id'
-            },
-            {
-                name: 'origin_id',
-                type: 'int'
-            },
-            {
-                name: 'description'
-            },
-            {
-                defaultValue: 0,
-                name: 'duration',
-                type: 'float'
-            },
-            {
-                name: 'rang'
-            },
-            {
-                dateFormat: 'timestamp',
-                name: 'date',
-                type: 'date'
-            },
-            {
-                name: 'app_id'
-            }
-        ]
-    }
+
+    fields: [
+        {
+            name: 'id'
+        },
+        {
+            name: 'local_id'
+        },
+        {
+            name: 'line_id',
+            type: 'int',
+            critical: true
+        },
+        {
+            name: 'origin_id',
+            type: 'int'
+        },
+        {
+            name: 'description'
+        },
+        {
+            defaultValue: 0,
+            name: 'duration',
+            type: 'float'
+        },
+        {
+            name: 'rang'
+        },
+        {
+            dateFormat: 'timestamp',
+            name: 'date',
+            type: 'date'
+        },
+        {
+            name: 'app_id'
+        }
+    ]
 });
